58fd7a618338d2c4f143eff3be0d07ba2ac89531
Kanybek Almanbet <kanyalmanbet@gmail.com>
2022-09-06 23:41:40 -0500

    asyncJS

---
+++ app/asyncawait.js
@@ -0,0 +1,8 @@
+const stocks = {
+  fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquids: ["water", "ice"],
+  holders: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
+
+const isShopOpen = true;
